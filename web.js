22c6ee6844b13cb36980dcaf85948c53ddf85074
jlurgo <jlurgo@gmail.com>
2018-04-09 12:24:49 -0300

    inicial

---
+++ web.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var http = require('http');
+var mongodb = require('mongodb');
+var _ = require("./underscore-min");
+
+var ObjectId = mongodb.ObjectID;
+
+//var uri = 'mongodb://127.0.0.1/Sime';
+var uri = 'mongodb://admin:energetico@ds239439.mlab.com:39439/energias';
+
+var app = express();
+var server = http.createServer(app);
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+app.use(allowCrossDomain);
+
+var puerto = process.env.PORT || 3000;
+server.listen(puerto);    
+
+console.log('Servidor vortex escuchando en puerto ' + puerto);
+
+mongodb.MongoClient.connect(uri, function(err, db) {  
+    app.get('/getMediciones/:medidor', function(request, response){
+		var medidor = request.params.medidor.toString();
+		
+		var col_mediciones = db.collection('mediciones');
+		col_mediciones.find({}).toArray(function(err, mediciones){
+			response.send(JSON.stringify(mediciones));
+		});	
+	});
+    
+    app.post('/guardarMedicion', function(request, response){
+		var medicion = request.body.medicion;
+		
+		db.collection('mediciones').save(medicion, function(){
+			if(err) throw err;
+			response.send("ok");
+		});
+	});	
+});
+
